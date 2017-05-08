9c90b9dfd434c10cb5ac0eb42a975e5a4d58e1fc
Hassan-Adel <hasanadel555@hotmail.com>
2017-05-08 03:26:15 +0200

    replacing mongodb with mongoose

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+var Message = mongoose.model('Message',{
+   msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -13,7 +18,25 @@ app.use(function(request,response,next){
 
 app.post('/api/message', function(request,response){
     console.log(request.body);
+
+    var message = new Message(request.body);
+    message.save();
+
     response.status(200);
+})
+
+function GetMessages(){
+    Message.find({}).exec(function(error,result){
+        console.log(result);
+    })
+}
+
+//DB Name = test
+mongoose.connect('mongodb://localhost:27017/test', function(error,db){
+    if(!error){
+        console.log('we are connected to mongo');
+        GetMessages();
+    }
 })
 
 var server = app.listen(5000, function(){
