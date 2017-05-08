27865a68324db63c196dcd87ea8800d50c787af1
Hassan-Adel <hasanadel555@hotmail.com>
2017-05-08 02:36:55 +0200

    Enabling CORS

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+//Allow CORS(Cross Origin Resource Sharing)
+app.use(function(request,response,next){
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+})
+
 app.post('/api/message', function(request,response){
     console.log(request.body);
     response.status(200);
