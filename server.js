4f598d2307a9c5a418f386753954405b9ae0481e
Hassan-Adel <hasanadel555@hotmail.com>
2017-05-10 03:09:46 +0200

    Moving models and controllers out of server.js and modifing the changes

--- server.js
+++ server.js
@@ -3,45 +3,37 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-var Message = mongoose.model('Message',{
-   msg: String
-});
+
+
+//Controllers
+var auth = require('./controllers/authCtrl');
+var message = require('./controllers/messageCtrl');
 
 app.use(bodyParser.json());
 
 //Allow CORS(Cross Origin Resource Sharing)
-app.use(function(request,response,next){
+app.use(function (request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 })
 
+//POST
+app.post('/authentication/register', auth.register);
+
 //GET
-app.get('/api/message', GetMessages);
+app.get('/api/message', message.get);
 
 //POST
-app.post('/api/message', function(request,response){
-    console.log(request.body);
-
-    var message = new Message(request.body);
-    message.save();
-
-    response.status(200);
-})
-
-function GetMessages(request,response){
-    Message.find({}).exec(function(error,result){
-        response.send(result);
-    })
-}
+app.post('/api/message', message.post);
 
 //DB Name = test
-mongoose.connect('mongodb://localhost:27017/test', function(error,db){
-    if(!error){
+mongoose.connect('mongodb://localhost:27017/test', function (error, db) {
+    if (!error) {
         console.log('we are connected to mongo');
     }
 })
 
-var server = app.listen(5000, function(){
-    console.log('listing on port ',server.address().port);
+var server = app.listen(5000, function () {
+    console.log('listing on port ', server.address().port);
 })
